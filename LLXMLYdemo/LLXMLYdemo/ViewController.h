a76cf726f3460bd558806ee8c87fcaa42fe3ed48
邓成成 <chengcheng.deng@baidao.com>
2017-09-05 11:09:08 +0800

    初始化工程

---
+++ LLXMLYdemo/LLXMLYdemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LLXMLYdemo
+//
+//  Created by 邓成成 on 2017/9/5.
+//  Copyright © 2017年 邓成成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LLXMLYdemo/LLXMLYdemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  LLXMLYdemo
+//
+//  Created by 邓成成 on 2017/9/5.
+//  Copyright © 2017年 邓成成. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
